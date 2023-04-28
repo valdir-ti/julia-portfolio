91143a26b2b31287e676c37a3dfe6508e8b7830c
Valdir Silva <valdir.ti@gmail.com>
2023-04-28 00:28:02 -0300

    feat: change font and wa me message

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,18 @@ function App() {
 
       <div className='absolute top-2 left-0 w-full'>
         <div className='flex justify-center flex-col items-center mt-6'>
-          <h2 className='font-bold text-3xl'>Julia Felix</h2>
+          <h2 className='font-bold text-6xl font-dancing'>Julia Felix</h2>
           <h3 className='mt-1'>Designer de sobrancelhas</h3>
         </div>
         <div className='flex justify-center mt-28 h-[160px]'>
           <img src={Image} alt="Julia image" width={150} height={150} className='rounded-full border-emerald-200 border-2'/>
         </div>
         <div className='flex flex-col items-center mt-28'>
-          <div className='flex justify-start items-center w-80'>
+          <div className='flex justify-between items-center w-70'>
             <img src={Zap} alt="zap" width={50} height={50} />
-            <a href='https://wa.me/5511977574635?text=Conversar%20com%20Julia%20do%20Studio%20Felix' target='_blank' className='font-bold underline'>WhattsApp | Agendamentos</a>
+            <a href='https://wa.me/5511977574635?text=Seja%20bem-vinda%20ao%20Studio%20Felix,%20eu%20sou%20Julia!!%20%20Para%20agilizar%20o%20seu%20atendimento%20me%20diga%20o%20seu%20nome%20e%20qual%20procedimento%20deseja%20realizar.' target='_blank' className='font-bold underline'>WhattsApp | Agendamentos</a>
           </div>
-          <div className='flex items-center justify-start w-80 mt-10 ml-4'>
+          <div className='flex items-center justify-between w-70 mt-10 ml-4'>
             <img src={Insta} alt="Instagram" width={30} height={30} />
             <a href='https://instagram.com/studioo_felix?igshid=ZWIzMWE5ZmU3Zg==' target='_blank' className='ml-2 font-bold underline'>Instagram</a>
           </div>
